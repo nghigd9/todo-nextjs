a472fdf72d4159788261ae32dffcb85c1a205f52
nghigd9 <nghigd9@gmail.com>
2022-10-26 11:16:12 +0700

    update mongo sv

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,17 +1,22 @@
-import mongoose  from "mongoose";
+import mongoose from 'mongoose';
+//  M*h%HHEre2iO
 
-const DB_URL = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
+// const DB_URL = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
+// const DB_URL = 'mongodb://nghi:M*h%HHEre2iO@cluster0.rwpca.mongodb.net/test';
+const uri =
+    'mongodb+srv://nghi:nghi1234@cluster0.rwpca.mongodb.net/?retryWrites=true&w=majority';
+const DB_URL = uri;
 
 if (!DB_URL) {
-	throw new Error(
-		"Please define the DB_URL environment variable inside .env.local"
-	);
+    throw new Error(
+        'Please define the DB_URL environment variable inside .env.local'
+    );
 }
 
 let cached = global.mongoose;
 
 if (!cached) {
-    cached =  global.mongoose = {conn : null, promise : null};
+    cached = global.mongoose = { conn: null, promise: null };
 }
 
 const dbConnect = async () => {
@@ -21,17 +26,15 @@ const dbConnect = async () => {
 
     if (!cached.promise) {
         const options = {
-            useNewUrlParser : true,
-            useUnifiedTopology : true,
-        }
-        cached.promise = mongoose.connect(DB_URL, options)
-            .then(mongoose => {
-                return mongoose;
-            })
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+        };
+        cached.promise = mongoose.connect(DB_URL, options).then((mongoose) => {
+            return mongoose;
+        });
     }
     cached.conn = await cached.promise;
     return cached.conn;
-}
+};
 
 export default dbConnect;
-
