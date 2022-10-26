eb0c2e6683aab946a26d65b7aa92e02ad21eb31c
nghigd9 <nghigd9@gmail.com>
2022-10-26 11:26:34 +0700

    domain

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,8 @@ import TextField from '@atlaskit/textfield';
 import EditFilledIcon from '@atlaskit/icon/glyph/edit-filled';
 import TrashIcon from '@atlaskit/icon/glyph/trash';
 
-const url = 'http://localhost:3000/api/task';
+// const url = 'http://localhost:3000/api/task';
+const url = 'https://todo-nextjs-pi.vercel.app/';
 
 export default function Home(props) {
     const [tasks, setTasks] = useState(props.tasks);
